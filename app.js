2a8e4e52e2228a05c3ad527b7bcb3dae5ea98ffa
marcodm-1 <marcodiazmoore@gmail.com>
2021-08-08 16:44:47 -0400

    Index view fetch setup

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const express = require("express");
+const fs = require("fs");
 let app = express();
 
 app.get("/", function(request, response){
-    response.send("test demo");
+    response.sendFile(
+        "views/index.html",
+        {"root": "./"},
+        function(err){
+            if(err){
+                console.log("Error occured: ", err);
+                response.send("Error occurred when trying to retrieve view from server")
+            }
+        });
+    console.log("index sent");
 })
 
 app.listen(3000);
