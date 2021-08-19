1c545792a11a88b254bad63044906eb854302173
marcodm-1 <marcodiazmoore@gmail.com>
2021-08-19 11:29:30 -0400

    Discover page js addition

--- views/js/discoverPage.js
+++ views/js/discoverPage.js
@@ -1,3 +1,15 @@
 // Client script that populates the discover page with data.
 
-let discoverData = await fetch();
+// localhost:3000/js/discoverpage.js
+
+async function getData(){
+    let discoverData = await fetch("../api/getdiscover");
+    return discoverData.json();
+}
+
+
+getData().then(function(data){
+        console.log("fetch properly got data: ", JSON.stringify(data));
+    }.catch(function(err){
+        console.log("Fetch didnt get data: ", err);
+    }))
