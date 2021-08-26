136b4b0c0808ab291191f521f352c1a9a254e66a
marcodm-1 <marcodiazmoore@gmail.com>
2021-08-26 15:04:55 -0400

    URL parsing function

--- views/js/PostPage.js
+++ views/js/PostPage.js
@@ -1,7 +1,39 @@
-function postComment(comment){
-    
+let subReadit;
+let postId;
+
+function createCommentElement(name, date, likes, description){
+    // likes = [upvote, downvote]
 }
 
+/*
+
+/api/sub/postLike/:subName/:postId/-1 = original post
+/api/sub/postLike/:subName/:postId/:commentId
+/api/sub/postComment/:subName/:postId
+
+*/
+function postComment(comment){
+
+}
+
+function parsePostURL(URL){
+    URL = URL.substring(URL.search("/r/") + 3); // Remove unneeded start of website URL
+	let breaks = [];
+	for(let i = 0; i < URL.length; i++){
+		if(URL[i] == '/'){
+			breaks.push(i);
+		}
+	}
+    return [URL.substring(0, breaks[0]), URL.substring(breaks[1] + 1)];
+}
+
+function init(){
+    let parsedURL = parsePostURL(document.URL);
+    subReadit = parsedURL[0];
+    postId = parsedURL[1];
+}
+
+init();
 /*
 Comment Template
 
