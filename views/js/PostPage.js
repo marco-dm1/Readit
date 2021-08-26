0c113225a42026e32e094b5571a9753ea1e9305e
marcodm-1 <marcodiazmoore@gmail.com>
2021-08-26 12:15:39 -0400

    Initial commit

---
+++ views/js/PostPage.js
@@ -0,0 +1,19 @@
+function postComment(comment){
+    
+}
+
+/*
+Comment Template
+
+<div id="contentCard" class="postCard card rounded-3 shadow-sm">
+    <div class="card-body">
+        <div class="comment-title">
+            <i>testaccount - 0 Likes</i>
+            <button class="far fa-arrow-alt-circle-up like-button"></button>
+            <button class="far fa-arrow-alt-circle-down like-button"></button>
+        </div>
+        test body
+    </div>
+</div>
+
+*/
