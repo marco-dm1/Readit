021ac92d0b2702a40e7ac2902eb0056eadf56294
marcodm-1 <marcodiazmoore@gmail.com>
2021-08-28 00:50:52 -0400

    accounts.js URL routing

--- routes/javascript.js
+++ routes/javascript.js
@@ -14,5 +14,53 @@ router.get("/discoverpage.js", function(req, res){
         });
 })
 
+router.get("/subreaditpage.js", function(req, res){
+    res.sendFile(
+        "subReaditPage.js",
+        {"root": "./views/js/"},
+        function(err){
+            if(err){
+                console.log("Error occured: ", err);
+                res.send("Error occurred when trying to retrieve view from server")
+            }
+        });
+})
+
+router.get("/postpage.js", function(req, res){
+    res.sendFile(
+        "postPage.js",
+        {"root": "./views/js/"},
+        function(err){
+            if(err){
+                console.log("Error occured: ", err);
+                res.send("Error occurred when trying to retrieve view from server")
+            }
+        });
+})
+
+router.get("/authentication.js", function(req, res){
+    res.sendFile(
+        "authentication.js",
+        {"root": "./views/js/"},
+        function(err){
+            if(err){
+                console.log("Error occured: ", err);
+                res.send("Error occurred when trying to retrieve view from server")
+            }
+        });
+})
+
+router.get("/accounts.js", function(req, res){
+    res.sendFile(
+        "accounts.js",
+        {"root": "./views/js/"},
+        function(err){
+            if(err){
+                console.log("Error occured: ", err);
+                res.send("Error occurred when trying to retrieve view from server")
+            }
+        });
+})
+
 // Export router for use in the primary routing js file
 module.exports = router;
