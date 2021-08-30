4f48b4e883349b8a95cb5642ff6835786e98be4c
marcodm-1 <marcodiazmoore@gmail.com>
2021-08-30 13:54:09 -0400

    getLogin API endpoint

--- routes/endpoints/acc.js
+++ routes/endpoints/acc.js
@@ -5,10 +5,17 @@ const router = require("express").Router();
 /*
 
 /api/acc/getPersonalData
-/api/acc/postLogin
+/api/acc/getLogin
 /api/acc/postRegister
 
 /api/acc/getProfileData/:userId
 
 */
+
+router.get("/getLogin", function(req, res){
+    res.set("Content-Type", "application/json");
+    console.log("get log in called");
+    res.send(JSON.stringify({success: true, demo: "hello"}));
+})
+
 module.exports = router;
