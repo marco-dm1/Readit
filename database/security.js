b4e21c7ee91c2b9cb3ef56240a20fddb7e2a9fa0
marcodm-1 <marcodiazmoore@gmail.com>
2021-09-01 15:04:05 -0400

    Security related functions for database functions

---
+++ database/security.js
@@ -0,0 +1,34 @@
+const bcrypt = require("bcrypt");
+let characters = "qwertyuioplkjhgfdsazxcvbnmMNBVCXZASDFGHJKLPOIUYTREWQ1234567890";
+
+async function createSessionToken(userId){
+    let sessionToken = "";
+    for(let i = 0; i < 50; i++){
+        sessionToken = sessionToken + characters[Math.round(Math.random() * characters.length)];
+    }
+    return sessionToken;
+}
+
+async function hashPassword(password){
+    return bcrypt.hash(password, 5, function(err, hash) {
+        if(err){
+            return false;
+        }else{
+            return hash;
+        }
+    });
+}
+
+async function comparePasswords(plaintextPassword, hashedPassword){
+    return bcrypt.compare(plaintextPassword, hashedPassword, function(err, result) {
+        if(err){
+            return false;
+        }else{
+            return result;
+        }
+    });
+}
+
+module.exports.createSessionToken = createSessionToken;
+module.exports.hashPassword = hashPassword;
+module.exports.comparePasswords = comparePasswords;
