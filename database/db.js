23d2f4958ea9c9fa9324fce2837ab12f66449b0a
marcodm-1 <marcodiazmoore@gmail.com>
2021-09-01 16:01:17 -0400

    Removed old db files

--- database/db.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose");
-
-mongoose.connect("mongodb://localhost:27017/siteData", {useNewUrlParser: true, useUnifiedTopology: true})
-
-const schemas = require("./schemas.js");
-
-console.log(schemas.discoverModel);
--- database/models.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const discoverModel = new mongoose.Schema({
-    topSubs: {type: Array, required: true},
-    latestPosts: {type: Array, required: true},
-    newestUser: {type: String, required: true}
-})
-
-module.exports.discoverModel = mongoose.model("discoverModel", discoverModel);
