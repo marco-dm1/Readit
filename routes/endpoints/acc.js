acc0a85c8243bbeb4935b1deb105a410a0b69311
marcodm-1 <marcodiazmoore@gmail.com>
2021-09-02 14:03:24 -0400

    Login and Register endpoints

--- routes/endpoints/acc.js
+++ routes/endpoints/acc.js
@@ -1,21 +1,35 @@
 // Handles express routing for the account API endpoints
 
 const router = require("express").Router();
+const db = require("../../database/functions.js");
 
 /*
-
 /api/acc/getPersonalData
 /api/acc/getLogin
 /api/acc/postRegister
-
 /api/acc/getProfileData/:userId
-
 */
 
-router.get("/getLogin", function(req, res){
+router.get("/getLogin", async function(req, res){
     res.set("Content-Type", "application/json");
-    console.log("get log in called");
-    res.send(JSON.stringify({success: true, demo: "hello"}));
+    console.log(`get log in called, user: ${req.headers["username"]} pass: ${req.headers["password"]}`);
+    let login = await db.checkLogin(req.headers["username"], req.headers["password"])
+    if(login != false){
+        res.send(JSON.stringify(login));
+    }else{
+        res.send(JSON.stringify({success: false}));
+    }
+})
+
+router.get("/postRegister", async function(req, res){
+    res.set("Content-Type", "application/json");
+    console.log("post register called");
+    let register = await db.registerAccount(req.headers["username"], req.headers["password"])
+    if(register != false){
+        res.send(JSON.stringify(register));
+    }else{
+        res.send(JSON.stringify({success: false}));
+    }
 })
 
 module.exports = router;
