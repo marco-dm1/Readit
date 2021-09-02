711885bc96aa1486193aefc83dcc8c6a89cd4d20
marcodm-1 <marcodiazmoore@gmail.com>
2021-09-02 14:09:35 -0400

    Logout button functionality

--- views/js/authentication.js
+++ views/js/authentication.js
@@ -1,6 +1,6 @@
 // Handles all client side authentication
 
-let sessionToken = document.cookie
+let sessionToken;
 
 function redirectURLs(){
     let redirects = [["accounts/login", 1], ["accounts/register", 1]]
@@ -20,9 +20,30 @@ function redirectURLs(){
                         path = "../";
                         break;
                 }
-                window.location.replace(path);
+                //window.location.replace(path + "accounts/user/me");
                 // document.cookie = "DO-NOT-SHARE-SECURE=test; expires=Thu, 18 Dec 2013 12:00:00 UTC"
             }
+        }
+    }
+}
+
+function logoutClick(){
+    // Set the session took cookie to expire and reload the page
+    if(sessionToken != null){
+        document.cookie = "sessionToken=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+        location.reload();
+    }
+}
+
+function logoutInit(){
+    // Find and set the session token if it exists
+    let rawCookie = document.cookie;
+    let tokenPosition = rawCookie.search("DO-NOT-SHARE-SECURE=");
+    if(tokenPosition != -1){
+        sessionToken = rawCookie.substring(20);
+        let logoutButton = document.getElementById("logoutButton");
+        if(logoutButton){
+            logoutButton.style.display = "block"; // Makes the logout button visible
         }
     }
 }
