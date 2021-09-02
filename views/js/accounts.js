131ed697aece651918b51c8d845a249191261afa
marcodm-1 <marcodiazmoore@gmail.com>
2021-09-02 11:39:27 -0400

    Login functionality and cookie expiration removal

--- views/js/accounts.js
+++ views/js/accounts.js
@@ -17,15 +17,52 @@ function handleRequest(url, params){
     })
 }
 
+function showNotification(classInput, messageInput){
+    let notificationBox = document.getElementById("notificationBox");
+    notificationBox.classList.remove("alert-danger");
+    notificationBox.classList.remove("alert-success");
+    notificationBox.classList.add(classInput);
+    notificationBox.innerHTML = messageInput;
+    notificationBox.style.display = "block";
+}
+
 async function login(handleInput, passwordInput){
     console.log(`Attempting to log in with handle: ${handleInput} & password: ${passwordInput}`);
     // Send a request to the server and await for the Promise's result
-    let result = await handleRequest("../api/acc/getLogin", {username: "test1", password: "test2"});
-    console.log(result);
+    if(handleInput != "" && passwordInput != ""){
+        let result = await handleRequest("../api/acc/getLogin", {username:  String(handleInput), password: String(passwordInput)});
+        if(result["success"] == true){
+            let tokenExpiration = new Date().toUTCString();
+            /*
+            let date = new Date();
+            console.log(date.toUTCString());
+            date.setMinutes(date.getMinutes()+15);
+            console.log(date.toUTCString());
+            */
+            let date = new Date();
+            date.setDate(date.getDate() + 1);
+            //document.cookie = `DO-NOT-SHARE-SECURE=${result["token"]}; expires=${date.toUTCString()}`
+            document.cookie = `DO-NOT-SHARE-SECURE=${result["token"]};`;
+            showNotification("alert-success", "Successfully logged in, redirecting...")
+            setTimeout(function(){
+                location.href = "../discover";
+            }, 2000)
+        }else{
+            if(result["message"] != null){
+                showNotification("alert-danger", result["message"]);
+            }else{
+                showNotification("alert-danger", "An error occurred.");
+            }
+        }
+    }else{
+        showNotification("alert-danger", "Your inputs can not be empty");
+    }
 }
 
 function register(handleInput, passwordInput, confirmPasswordInput){
     if(passwordInput === confirmPasswordInput){
         console.log(`Attempting to register with the handle: ${handleInput} & password: ${passwordInput}`);
+    }else{
+        showNotification("alert-warning", "Your passwords do not match.");
     }
 }
