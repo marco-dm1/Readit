82f25df3aa092ef3f48b455a316471be3d1fb428
marcodm-1 <marcodiazmoore@gmail.com>
2021-09-02 14:06:44 -0400

    Origin post comment clarification comment

--- routes/endpoints/sub.js
+++ routes/endpoints/sub.js
@@ -6,8 +6,9 @@ const router = require("express").Router();
 
 /api/sub/getSub/:subName
 /api/sub/getPost/:subName/:postId
-/api/sub/postLike/:subName/:postId
+/api/sub/postLike/:subName/:postId/-1 = original post comment
 /api/sub/postLike/:subName/:postId/:commentId
+/api/sub/postComment/:subName/:postId
 
 */
 module.exports = router;
