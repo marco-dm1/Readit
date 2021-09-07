47bebedc0e61519f24479be6eb31eb627e54caae
marcodm-1 <marcodiazmoore@gmail.com>
2021-09-06 23:44:41 -0400

    Login/Register queries and comment explanation addition

--- database/query.js
+++ database/query.js
@@ -1,19 +1,17 @@
+// Script that handles all the MongoDB database's queries
+
 async function checkIfExists(database, usernameInput, returnProjection){
-    return database.collection("users").find({name: usernameInput}, {projection: returnProjection}).toArray().then(function(data){
+    return database.collection("users").find({name: usernameInput}/*, {projection: returnProjection}*/).toArray().then(function(data){
         return data[0];
     }).catch(function(err){
-        console.log(`Database Query checkIfExists error: ${err}`);
         return false;
     })
 }
 
 async function saveAccount(datebase, usernameInput, hashedPassword, sessionToken, joinDate){
-    return datebase.collection("users").insertOne({name: usernameInput, password: hashedPassword, session: sessionToken, joined: joinDate}, function(err, res){
+    datebase.collection("users").insertOne({name: usernameInput, password: hashedPassword, session: sessionToken, joined: joinDate}, function(err, res){
         if(err){
             console.log(`Database query saveAccount error: ${err}`);
-            return false;
-        }else{
-            return true;
         }
     })
 }
@@ -21,7 +19,7 @@ async function saveAccount(datebase, usernameInput, hashedPassword, sessionToken
 async function updateSessionToken(datebase, username, newToken){
     return datebase.collection("users").updateOne({name: username}, { $set: {session: newToken} }, function(err) {
         if (err){
-            console.log(`Database Query updateSessionToken error: ${err}`);
+            console.log(`Database query updateSessionToken error: ${err}`);
             return false;
         };
         return true;
