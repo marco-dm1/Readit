d2aa5555554b9e55d1a52bc79429e4c33ddccad3
marcodm-1 <marcodiazmoore@gmail.com>
2021-09-21 17:33:43 -0400

    API endpoint demo data addition

--- routes/endpoints/sub.js
+++ routes/endpoints/sub.js
@@ -11,4 +11,29 @@ const router = require("express").Router();
 /api/sub/postComment/:subName/:postId
 
 */
+
+router.get("/getSub/:subName", async function(req, res){
+    res.set("Content-Type", "application/json");
+    /*
+    let login = await db.checkLogin(req.headers["username"], req.headers["password"])
+    if(login != false){
+        res.send(JSON.stringify(login));
+    }else{
+        res.send(JSON.stringify({success: false}));
+    }
+    */
+   let data = [
+       {userName: "userName", postId: "123", postTitle: "test", postBody: "this is the body"},
+       {userName: "userName", postId: "123", postTitle: "test", postBody: "this is the body"},
+       {userName: "userName", postId: "123", postTitle: "test", postBody: "this is the body"},
+       {userName: "userName", postId: "123", postTitle: "test", postBody: "this is the body"},
+       {userName: "userName", postId: "123", postTitle: "test", postBody: "this is the body"},
+       {userName: "userName", postId: "123", postTitle: "test", postBody: "this is the body"},
+       {userName: "userName", postId: "123", postTitle: "test", postBody: "this is the body"},
+       {userName: "userName", postId: "123", postTitle: "test", postBody: "this is the body"},
+   ]
+   res.send(JSON.stringify(data))
+})
+
+
 module.exports = router;
