034a84a543e5afa957111229778035ec188d8ff4
marcodm-1 <marcodiazmoore@gmail.com>
2021-09-21 00:09:38 -0400

    findSubReadit and initial commit of post fetch function

--- views/js/subReaditPage.js
+++ views/js/subReaditPage.js
@@ -1,5 +1,29 @@
-// Client script that handles sub readit filtering
+// Client script that handles sub readit page functionality
+
+let subreadit;
 
 function filterUpdate(filter){
     console.log(filter);
 }
+
+async function getSubReaditPosts(){
+    let getPostRequest = await fetch(`../api/sub/getSub/${subreadit}`);
+}
+
+async function findSubReadit(){
+    let path = document.location.pathname;
+    let directoryIndex = path.indexOf("/r/") + 3;
+    let slashIndex = path.substring(directoryIndex).indexOf('/');
+    if(slashIndex == -1){
+        subreadit = path.substring(directoryIndex);
+    }else{
+        subreadit = path.substring(directoryIndex, slashIndex + 3);
+    }
+}
+
+function init(){
+    await findSubReadit(); // Figure out where the client is on the site before calling dependant functions
+    getSubReaditPosts();
+}
+
+init();
