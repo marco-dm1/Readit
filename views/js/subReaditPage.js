819194fa801a93818efaaddc6ef8d51c2ea5db72
marcodm-1 <marcodiazmoore@gmail.com>
2021-09-21 16:53:26 -0400

    ContentCard cloning functionality for post data population

--- views/js/subReaditPage.js
+++ views/js/subReaditPage.js
@@ -6,14 +6,27 @@ function filterUpdate(filter){
     console.log(filter);
 }
 
-function createPosts(postsData){
+function visualizePosts(postsData){
+    let postContainer = document.getElementsById("postContainer");
+    let contentCardTemplate = document.getElementsById("contentCardTemplate");
 
+    function createDiv(user, id, title, body){
+        let templateClone = contentCardTemplate.cloneNode(true);
+        templateClone.id = "contentCard"
+        templateClone.querySelector(".card-header").querySelector("#contentCardTitle").innerText = title;
+        if(body.length > 50){
+            body = body.substring(0, 50) + "..";
+        }
+        templateClone.querySelector(".card-body").querySelector("#contentCardBody").innerText = body;
+        templateClone.style.display = "block";
+        postContainer.appendChild(templateClone);
+    }
 }
 
 async function getSubReaditPosts(){
     // Fetches the subReadit's data from the server and calls another function to visualize it
     let postsRequest = await fetch(`../api/sub/getSub/${subreadit}`);
-    createPosts(postsRequest.json());
+    visualizePosts(postsRequest.json());
 }
 
 async function findSubReadit(){
